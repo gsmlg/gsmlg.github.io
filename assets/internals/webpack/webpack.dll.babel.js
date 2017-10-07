878a0eb365a78f68a93805a993d834f542a8820d
gsmlg <gsmlg@gsmlg.com>
2017-10-08 02:03:08 +0800

    init elixir project

---
+++ assets/internals/webpack/webpack.dll.babel.js
@@ -0,0 +1,41 @@
+/**
+ * WEBPACK DLL GENERATOR
+ *
+ * This profile is used to cache webpack's module
+ * contexts for external library and framework type
+ * dependencies which will usually not change often enough
+ * to warrant building them from scratch every time we use
+ * the webpack process.
+ */
+
+const { join } = require('path');
+const defaults = require('lodash/defaultsDeep');
+const webpack = require('webpack');
+const config = require('../config');
+const pkg = require(join(config.root, 'package.json'));
+const dllPlugin = require('../config').dllPlugin;
+
+if (!pkg.dllPlugin) { process.exit(0); }
+
+const dllConfig = defaults(pkg.dllPlugin, dllPlugin.defaults);
+const outputPath = join(config.root, dllConfig.path);
+
+module.exports = require('./webpack.base.babel')({
+  context: config.root,
+  entry: dllConfig.dlls ? dllConfig.dlls : dllPlugin.entry(pkg),
+  devtool: 'eval',
+  output: {
+    filename: '[name].dll.js',
+    path: outputPath,
+    library: '[name]',
+  },
+  plugins: [
+    new webpack.DllPlugin({
+      name: '[name]',
+      path: join(outputPath, '[name].json'),
+    }),
+  ],
+  performance: {
+    hints: false,
+  },
+});
---
+++ lib/gsmlg_web/router.ex
@@ -0,0 +1,28 @@
+defmodule GsmlgWeb.Router do
+  use GsmlgWeb, :router
+
+  pipeline :browser do
+    plug :accepts, ["html"]
+    plug :fetch_session
+    plug :fetch_flash
+    plug :protect_from_forgery
+    plug :put_secure_browser_headers
+  end
+
+  pipeline :api do
+    plug :accepts, ["json"]
+  end
+
+  scope "/", GsmlgWeb do
+    pipe_through :browser # Use the default browser stack
+
+    get "/", PageController, :index
+
+    get "/*path", PageController, :not_found
+  end
+
+  # Other scopes may use custom stacks.
+  scope "/api", GsmlgWeb do
+    pipe_through :api
+  end
+end
