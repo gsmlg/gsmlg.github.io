278ff4c45ce16843150fb898aca1beb09b18ca2c
gsmlg <gsmlg@gsmlg.com>
2017-11-15 23:29:11 +0800

    fix

--- lib/gsmlg_web/controllers/page_controller.ex
+++ lib/gsmlg_web/controllers/page_controller.ex
@@ -4,12 +4,12 @@ defmodule GsmlgWeb.PageController do
   def index(conn, _params) do
     conn
     |> put_layout(false)
-    |> render "index.html"
+    |> render("index.html")
   end
 
   def not_found(conn, _params) do
     conn
     |> put_layout(false)
-    |> render "index.html"
+    |> render("index.html")
   end
 end
--- lib/gsmlg_web/views/page_view.ex
+++ lib/gsmlg_web/views/page_view.ex
@@ -1,7 +1,7 @@
 defmodule GsmlgWeb.PageView do
   use GsmlgWeb, :view
 
-  def render("index.html", assigns) do
+  def render("index.html", _assigns) do
     index_html = Path.join([__DIR__, "..", "..", "..", "priv", "static", "index.html"])
     html = File.read!(index_html)
     {:safe, html}
