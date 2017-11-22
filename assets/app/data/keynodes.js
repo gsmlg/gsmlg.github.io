da0e474012ff1910ca60f199d7d3300192a80707
gsmlg <gsmlg@gsmlg.com>
2017-11-23 00:54:27 +0800

    add dynamic import

--- assets/app/data/keynodes.js
+++ assets/app/data/keynodes.js
@@ -1,22 +1,28 @@
-var keynotes = [
+const keynotes = [
   {
     id: 1,
-    name: "BIG WEB APP? COMPILE IT!",
-    author: "ALON ZAKAI / MOZILLA",
-    link: "https://kripken.github.io/mloc_emscripten_talk"
+    name: 'BIG WEB APP? COMPILE IT!',
+    author: 'ALON ZAKAI / MOZILLA',
+    link: 'https://kripken.github.io/mloc_emscripten_talk',
   },
   {
     id: 2,
-    name: "Debug in Webpack",
-    author: "gsmlg",
-    link: "https://gsmlg.github.io/Reveal/debug-module/"
+    name: 'Debug in Webpack',
+    author: 'gsmlg',
+    link: 'https://gsmlg.github.io/Reveal/debug-module/',
   },
   {
     id: 3,
-    name: "Promise A Plus",
-    author: "gsmlg",
-    link: "https://gsmlg.github.io/Reveal/promise-a-plus/dist/"
-  }
+    name: 'Promise A Plus',
+    author: 'gsmlg',
+    link: 'https://gsmlg.github.io/Reveal/promise-a-plus/dist/',
+  },
+  {
+    id: 4,
+    name: 'Dynamic Import',
+    author: 'gsmlg',
+    link: 'https://gsmlg.github.io/Reveal/dynamic-import/dist/',
+  },
 ];
 
 export default keynotes;
