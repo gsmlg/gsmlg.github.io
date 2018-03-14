124bfea278a4065c2f0cc595cb2ea0f64ef68f9c
gsmlg <gsmlg@gsmlg.com>
2018-03-14 22:07:50 +0800

    all drop check done

--- assets/app/containers/ChineseChess/index.js
+++ assets/app/containers/ChineseChess/index.js
@@ -29,12 +29,81 @@ const canDrop = (item, pos, pieces) => {
   const target = _.find(pieces, { position: pos });
   if (target && target.color === item.color) return false;
 
+  const hasntPiece = (pos) => !_.find(pieces, { position: pos });
+
+  const rk = _.find(pieces, { color: 'red', type: 0 });
+  const bk = _.find(pieces, { color: 'black', type: 0 });
+  if (!rk || !bk) return false;
+  if (item.type !== 0 && rk.position.x === bk.position.x && fx === rk.position.x) {
+    let count = 0;
+    for (let i = rk.position.y + 1; i < bk.position.y; i ++) {
+      if (!hasntPiece({x: fx, y: i})) count++;
+    }
+    if (count === 1 && tx !== fx) return false;
+  }
+
   switch(item.type) {
     case 0: // 帅
+      if (tx < 3 || tx > 5) return false;
+      if (item.color === 'red' && (ty > 2)) return false;
+      if (item.color === 'black' && (ty < 7)) return false;
+      if (item.color == 'red') {
+        if (tx === bk.position.x) {
+          let count = 0;
+          for (let i = rk.position.y + 1; i < bk.position.y; i ++) {
+            if (!hasntPiece({x: tx, y: i})) count++;
+          }
+          if (count === 0) return false;
+        }
+      } else {
+        if (tx === rk.position.x) {
+          let count = 0;
+          for (let i = rk.position.y + 1; i < bk.position.y; i ++) {
+            if (!hasntPiece({x: tx, y: i})) count++;
+          }
+          if (count === 0) return false;
+        }
+      }
+      if (tx === fx && (ty === fy + 1 || ty === fy - 1)) return true;
+      if (ty === fy && (tx === fx + 1 || tx === fx - 1)) return true;
+      return false;
     case 1: // 士
+      if (tx < 3 || tx > 5) return false;
+      if (item.color === 'red' && (ty > 2)) return false;
+      if (item.color === 'black' && (ty < 7)) return false;
+      if (tx === fx + 1 && (ty === fy + 1 || ty === fy - 1)) return true;
+      if (tx === fx - 1 && (ty === fy + 1 || ty === fy - 1)) return true;
+      return false;
     case 2: // 相
+      if (item.color === 'red' && ty > 4) return false;
+      if (item.color === 'black' && ty < 5) return false;
+      if (hasntPiece({x: fx-1, y: fy-1})) {
+        if (tx === fx-2 && ty === fy-2) return true;
+      }
+      if (hasntPiece({x: fx-1, y: fy+1})) {
+        if (tx === fx-2 && ty === fy+2) return true;
+      }
+      if (hasntPiece({x: fx+1, y: fy-1})) {
+        if (tx === fx+2 && ty === fy-2) return true;
+      }
+      if (hasntPiece({x: fx+1, y: fy+1})) {
+        if (tx === fx+2 && ty === fy+2) return true;
+      }
+      return false;
     case 3: // 马
-      return true;
+      if (hasntPiece({x:fx-1, y: fy})) {
+        if (tx === fx-2 && (ty === fy + 1 || ty === fy - 1)) return true;
+      }
+      if (hasntPiece({x:fx+1, y: fy})) {
+        if (tx === fx+2 && (ty === fy + 1 || ty === fy - 1)) return true;
+      }
+      if (hasntPiece({x:fx, y: fy-1})) {
+        if (ty === fy-2 && (tx === fx + 1 || tx === fx - 1)) return true;
+      }
+      if (hasntPiece({x:fx, y: fy+1})) {
+        if (ty === fy+2 && (tx === fx + 1 || tx === fx - 1)) return true;
+      }
+      return false;
     case 4: // 车
       if (fx === tx) {
         let count = 0;
