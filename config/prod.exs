237a848ad0b9f79f48ffcecf4808754089529d1e
gaoshiming <gaoshiming@zdns.cn>
2018-07-03 11:29:19 +0800

    remove check_origin

--- config/prod.exs
+++ config/prod.exs
@@ -15,7 +15,8 @@ use Mix.Config
 # which you typically run after static files are built.
 config :gsmlg, GsmlgWeb.Endpoint,
   load_from_system_env: true,
-  url: [port: 443]
+  url: [host: "gsmlg.org", port: 443],
+  check_origin: false
   # cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
