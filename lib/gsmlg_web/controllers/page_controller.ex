81543db3777abd27ad62da93eaed668cde698cce
gsmlg <gsmlg@gsmlg.com>
2018-07-20 22:41:22 +0800

    change for building

--- lib/gsmlg_web/controllers/page_controller.ex
+++ lib/gsmlg_web/controllers/page_controller.ex
@@ -1,15 +1,28 @@
 defmodule GsmlgWeb.PageController do
   use GsmlgWeb, :controller
+  plug :put_layout, false
 
   def index(conn, _params) do
-    conn
-    |> put_layout(false)
-    |> render("index.html")
+    if Mix.env == :prod do
+      conn
+      |> put_resp_content_type("text/html")
+      |> send_file(200, Path.join([Application.app_dir(:gsmlg), "priv", "static", "index.html"]))
+      |> halt()
+    else
+      conn
+      |> render("index.html")
+    end
   end
 
   def not_found(conn, _params) do
-    conn
-    |> put_layout(false)
-    |> render("index.html")
+    if Mix.env == :prod do
+      conn
+      |> put_resp_content_type("text/html")
+      |> send_file(404, Path.join([Application.app_dir(:gsmlg), "priv", "static", "index.html"]))
+      |> halt()
+    else
+      conn
+      |> render("index.html")
+    end
   end
 end
