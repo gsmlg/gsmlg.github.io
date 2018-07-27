58cf26a69ff5a61fd69e91ad7ad0fadbd13bc22f
gsmlg <me@gsmlg.org>
2018-07-28 02:20:17 +0800

    fix selectors and remvoe usless

--- assets/app/containers/DistributedNodePage/tests/reducer.test.js
+++
@@ -1,9 +0,0 @@
-
-import { fromJS } from 'immutable';
-import distributedNodePageReducer from '../reducer';
-
-describe('distributedNodePageReducer', () => {
-  it('returns the initial state', () => {
-    expect(distributedNodePageReducer(undefined, {})).toEqual(fromJS({}));
-  });
-});
--- assets/app/containers/DistributedNodePage/tests/selectors.test.js
+++
@@ -1,8 +0,0 @@
-// import { fromJS } from 'immutable';
-// import { selectDistributedNodePageDomain } from '../selectors';
-
-describe('selectDistributedNodePageDomain', () => {
-  it('Expect to have unit tests specified', () => {
-    expect(true).toEqual(false);
-  });
-});
