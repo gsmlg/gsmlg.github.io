854efc2182df6a1e0cf24c18d44c107a9b9fbd7d
gsmlg <me@gsmlg.org>
2018-07-28 17:15:10 +0800

    fix loading

--- assets/app/containers/NodePage/Loadable.js
+++ assets/app/containers/NodePage/Loadable.js
@@ -5,8 +5,9 @@
  */
 
 import Loadable from 'react-loadable';
+import LayoutLoading from 'components/Layout/loading';
 
 export default Loadable({
   loader: () => import(/* webpackChunkName: "NodePage" */'./index'),
-  loading: () => null,
+  loading: LayoutLoading,
 });
