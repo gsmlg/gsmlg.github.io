2e96f335859da62edd8202c90e3a771832c1a181
gsmlg <me@gsmlg.org>
2019-04-04 14:40:38 +0800

    udpate

--- mix.exs
+++ mix.exs
@@ -33,17 +33,17 @@ defmodule Gsmlg.Mixfile do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.3.0"},
+      {:phoenix, "~> 1.4.0"},
       {:phoenix_pubsub, "~> 1.0"},
       {:phoenix_ecto, "~> 3.2"},
-      {:ecto_mnesia, "~> 0.9.0"},
+      {:ecto_mnesia, "~> 0.9"},
       {:phoenix_html, "~> 2.10"},
       {:phoenix_live_reload, "~> 1.0", only: :dev},
       {:gettext, "~> 0.11"},
-      {:cowboy, "~> 1.0"},
-      {:guardian, "~> 1.0-beta"},
-      {:httpoison, "~> 0.11.1"},
-      {:poison, "~> 3.1.0"},
+      {:cowboy, "~> 2.0"},
+      {:guardian, "~> 1.0"},
+      {:httpoison, "~> 1.0"},
+      {:poison, "~> 3.0"},
       {:distillery, "~> 2.0-pre", runtime: false},
     ]
   end
