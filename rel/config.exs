611ab4f64df4cece8d45789f34cbc7317c2f4357
gsmlg <me@gsmlg.org>
2019-08-08 00:46:47 +0800

    update distillery config

--- rel/config.exs
+++ rel/config.exs
@@ -2,18 +2,19 @@
 # They can then be used by adding `plugin MyPlugin` to
 # either an environment, or release definition, where
 # `MyPlugin` is the name of the plugin module.
-Path.join(["rel", "plugins", "*.exs"])
+~w(rel plugins *.exs)
+|> Path.join()
 |> Path.wildcard()
 |> Enum.map(&Code.eval_file(&1))
 
-use Mix.Releases.Config,
-    # This sets the default release built by `mix release`
+use Distillery.Releases.Config,
+    # This sets the default release built by `mix distillery.release`
     default_release: :default,
-    # This sets the default environment used by `mix release`
+    # This sets the default environment used by `mix distillery.release`
     default_environment: Mix.env()
 
 # For a full list of config options for both releases
-# and environments, visit https://hexdocs.pm/distillery/configuration.html
+# and environments, visit https://hexdocs.pm/distillery/config/distillery.html
 
 
 # You may define one or more environments in this file,
@@ -30,18 +31,19 @@ environment :dev do
   # dev mode.
   set dev_mode: true
   set include_erts: false
-  set cookie: :"t7(>[!jtx/O$^vGaoXg}PF_<Z7bP[eNYw:0zIrzk3CM!Wg|!GkLGv]U8x;;~dfZu"
+  set cookie: :"7qBz=LGW^uUmb&ZP@|M^PMrLGiH?`*vIWuR9t^N_2pR_*BnhL>N1H)bj=sd@s!yu"
 end
 
 environment :prod do
   set include_erts: true
   set include_src: false
-  set cookie: :";:25C67a]Li9tCWp7tnz}:{V|]T2_p*9t)lS}/VE]zbZPN`4*LiA|oaim*jnSE9A"
+  set cookie: :"Y8KKf^;B&j:Ft$7SbS0O21$C!%!J)|YiedD@4BNQ:9I@.14L)9r^?T:bFh~]}6o>"
+  set vm_args: "rel/vm.args"
 end
 
 # You may define one or more releases in this file.
 # If you have not set a default release, or selected one
-# when running `mix release`, the first release in the file
+# when running `mix distillery.release`, the first release in the file
 # will be used by default
 
 release :gsmlg do
