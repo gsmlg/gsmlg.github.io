27a9002ef36d0fcbee1190f658486b7ecdd7334e
gsmlg <me@gsmlg.org>
2020-04-30 11:35:02 +0800

    update deps

--- mix.exs
+++ mix.exs
@@ -33,8 +33,8 @@ defmodule Gsmlg.Mixfile do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.4.0"},
-      {:phoenix_pubsub, "~> 1.0"},
+      {:phoenix, "~> 1.5.0"},
+      {:phoenix_pubsub, "~> 2.0"},
       {:phoenix_ecto, "~> 4.0"},
       {:etso, "~> 0.1.1"},
       {:phoenix_html, "~> 2.10"},
