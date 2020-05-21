973174db14e5677eb7e94b374a218ec682b7b72d
gsmlg <me@gsmlg.org>
2020-05-21 09:46:13 +0800

    add home page text

--- assets/app/containers/HomePage/index.js
+++ assets/app/containers/HomePage/index.js
@@ -25,10 +25,27 @@ const useStyles = makeStyles((theme) => ({
     paddingBottom: 16,
     margin: theme.spacing(3),
   }),
+  text: {
+    fontSize: '3rem',
+    transition: '1000ms all',
+  },
 }));
 
 const HomePage = (props) => { // eslint-disable-line react/prefer-stateless-function
   const classes = useStyles();
+  const [colors, setColors] = React.useState(['#000','#000','#000','#000']);
+  React.useEffect(() => {
+    const randomRGB= () => {
+      const r = (Math.random() * 10e16 % 0xff).toString(16);
+      const g = (Math.random() * 10e16 % 0xff).toString(16);
+      const b = (Math.random() * 10e16 % 0xff).toString(16);
+      return `#${r}${g}${b}`;
+    };
+    const t = setInterval(() => {
+      setColors([randomRGB(), randomRGB(), randomRGB(), randomRGB()]);
+    }, 2000);
+    return () => clearInterval(t);
+  }, []);
 
   return (
     <Layout>
@@ -39,8 +56,17 @@ const HomePage = (props) => { // eslint-disable-line react/prefer-stateless-func
         ]}
       />
       <Paper className={classes.root} elevation={4}>
-        <Typography type="headline" component="h3">
-          这里是我的主页！
+        <Typography type="headline" component="h3" className={classes.text} style={{color: colors[0]}}>
+            吾日三省吾身
+        </Typography>
+        <Typography type="headline" component="h3" className={classes.text} style={{color: colors[1]}}>
+            为人谋而不忠乎
+        </Typography>
+        <Typography type="headline" component="h3" className={classes.text} style={{color: colors[2]}}>
+            与朋友交而不信乎
+        </Typography>
+        <Typography type="headline" component="h3" className={classes.text} style={{color: colors[3]}}>
+            传不习乎
         </Typography>
       </Paper>
     </Layout>
